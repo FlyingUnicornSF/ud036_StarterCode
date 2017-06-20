407cfe021bac02c83dbac99aefc8c27b7be20e12
Flying Unicorn in SF <FlyingUnicornSF@users.noreply.github.com>
2017-06-19 20:10:30 -0700

    found https://gist.github.com/jxson/1784669 and made changes

--- media.py
+++ media.py
@@ -1,18 +1,9 @@
-# import webbrowser
-
-
-
-# _chrome_launcher = webbrowser.get('open -a /Applications/Google\ Chrome.app %s')
-
-
 class Movie():
-    """Creating Movie class"""
+    """Creating Movie class. Take movie title, story line, and
+    link to poster image and trailer youtube"""
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-# def show_trailer(self):
-#        _chrome_launcher.open(self.trailer_youtube_url)
